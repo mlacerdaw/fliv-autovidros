7a9dfc22e232d1472c56a715e040ba573b1368b9
Micael <mlacerdaw@gmail.com>
2025-08-05 19:47:58 -0300

    alt

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { createLogger, defineConfig } from 'vite';
 
+
 const configWindowFetchMonkeyPatch = `
 const originalFetch = window.fetch;
 
